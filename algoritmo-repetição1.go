cfbd4783658676eabd1bd949c21ff1fd66e243cf
Gabriel de Medeiros Lima <gabrilima2004@dinfor.uniceub>
2023-03-20 10:34:17 -0300

    solução Rp1

---
+++ algoritmo-repetição1.go
@@ -0,0 +1,14 @@
+// Faça um algoritmo que imprima os números de 1 a 10 em ordem crescente.
+package main
+
+import "fmt"
+
+func main() {
+	i := 1
+
+	for i <= 10 {
+		fmt.Println(i)
+		i++
+	}
+}
+
