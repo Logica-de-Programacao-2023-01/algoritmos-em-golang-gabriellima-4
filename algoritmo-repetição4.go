ffe33540f072fc00d75d99ea43998db1117045c4
Gabriel de Medeiros Lima <gabrilima2004@dinfor.uniceub>
2023-03-20 11:08:50 -0300

    solução Rp4

---
+++ algoritmo-repetição4.go
@@ -0,0 +1,17 @@
+// Faça um algoritmo que imprima os múltiplos de 3 de 0 a 30.
+package main
+
+import "fmt"
+
+func main() {
+	i := 0
+
+	for i <= 30 {
+		fmt.Println(i)
+		i++
+		i++
+		i++
+
+	}
+}
+
